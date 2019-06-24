04396adbcba03c9e02ed2a043810fe16d779f4c3
Jonathan Linowes <linojon@gmail.com>
2019-06-24 10:06:10 -0400

    ch10 360 photos and videos

---
+++ Assets/_UVRP2/Ch10-360Media/Scripts/PlayPause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayPause : MonoBehaviour
+{
+    public string button = "Fire1";
+    private VideoPlayer player;
+
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown(button))
+        {
+            if (player.isPlaying)
+            {
+                player.Pause();
+            }
+            else
+            {
+                player.Play();
+            }
+        }
+    }
+}
