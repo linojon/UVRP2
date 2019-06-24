bd67577820c5f789394b844920973a9266557740
Jonathan Linowes <linojon@gmail.com>
2019-06-24 13:42:05 -0400

    ch11 blackbird story timeline

---
+++ Assets/_UVRP2/Ch11-Timeline/Scripts/LookAtToStart.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class LookAtToStart : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public float timeToSelect = 3f;
+    public GameObject countdownCanvas;
+    public Text coundownText;
+
+    private float countDown;
+    private bool playToSetup;
+    private double duration;
+    private bool resetSetup;
+
+    void Start()
+    {
+        countDown = timeToSelect;
+        resetSetup = true;
+        countdownCanvas.SetActive(false);
+     }
+
+    void Update()
+    {
+        if (timeline.state == PlayState.Playing)
+        {
+            return;
+        }
+        if (resetSetup)
+        {
+            StartCoroutine("PlayToSetup");
+            resetSetup = false;
+        }
+
+        // Is user looking here?
+        Transform camera = Camera.main.transform;
+        Ray ray = new Ray(camera.position, camera.rotation * Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && (hit.collider.gameObject == gameObject))
+        {
+            if (countDown > 0f)
+            {
+                countDown -= Time.deltaTime;
+                //Debug.Log("Counting" + countDown);
+                countdownCanvas.SetActive(true);
+                coundownText.text = countDown.ToString("F0");
+            }
+            else
+            {
+                // go!
+                //Debug.Log("Go!");
+                countdownCanvas.SetActive(false);
+                timeline.Play();
+                resetSetup = true;
+            }
+        }
+        else
+        {
+            // reset timer
+            //Debug.Log("ResetTimer");
+            countDown = timeToSelect;
+            countdownCanvas.SetActive(false);
+        }
+    }
+
+    IEnumerator PlayToSetup()
+    {
+        timeline.Play();
+        yield return new WaitForSeconds(0.1f);
+        timeline.Stop();
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+## Chapter 11 - Animation and VR Storytelling (Timeline) ##
+Scenes:
+* Ch11-1-Blackbird
+
+Credits:
+* Music: The Beatles and Paul McCartney song, "Blackbird" performed by guitarist
+Salvatore Manalo. Mp3 download: http://mp3freeget4.online/play/thebeatles-paul-mccartney-blackbird-cover/chSrubUUdwc.html
+* Scene and tree: Nature Starter Kit, https://assetstore.unity.com/packages/3d/environments/nature-starter-kit-1-49962
+* Nest and egg: Use the NestAndEgg prefab file provided with this book (source: tutorial using Cinema 4D: https://www.youtube.com/watch?v=jzoNZslTQfI,
+ .c4d file download, https://yadi.sk/d/ZQep-K-AMKAc8)
+* Living Birds: https://assetstore.unity.com/packages/3d/characters/animals/living-birds-15649
+* Wispy Skybox: https://assetstore.unity.com/packages/2d/texturesmaterials/sky/wispy-skybox-21737
+
