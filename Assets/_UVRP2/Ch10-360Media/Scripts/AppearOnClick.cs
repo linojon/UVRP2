189c364f11b41fdb029ab0eccda76cdf8ccde58d
Jonathan Linowes <linojon@gmail.com>
2019-06-24 00:50:41 -0400

    Ch10 spheres and globles

---
+++ Assets/_UVRP2/Ch10-360Media/Scripts/AppearOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearOnClick : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject handGlove;
+    public string button = "Fire1";
+
+    void Update()
+    {
+        target.SetActive(Input.GetButton(button));
+        handGlove.SetActive((!Input.GetButton(button)));
+    }
+}
